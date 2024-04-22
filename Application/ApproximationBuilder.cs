e945b41f16f976bbd3d2812e5d195acf37254368
Evgeny Khoroshavin <themplarer@gmail.com>
2024-04-22 22:41:00 +0500

    fixes

--- Application/ApproximationBuilder.cs
+++ Application/ApproximationBuilder.cs
@@ -25,49 +25,53 @@ public class ApproximationBuilder
 	private IEnumerable<PiecewiseFunction> GenerateLinearApproximations(PiecewiseFunction piecewiseFunction,
 		double newVariation)
 	{
-		var freeParamInterval =
-			new Interval<decimal>((decimal) -newVariation, (decimal) newVariation, IntervalInclusion.Closed);
+		return Build(new FunctionPart[piecewiseFunction.Parts.Length], 0, 0);
 
-		return Build(new FunctionPart[piecewiseFunction.Parts.Length], 0);
-
-		IEnumerable<PiecewiseFunction> Build(FunctionPart[] functionParts, int n)
+		Interval<decimal> GetFreeParameterRange()
 		{
-			if (piecewiseFunction.Parts.Length == n)
-			{
-				yield return new PiecewiseFunction(functionParts);
-			}
-			else if (n == 0)
-			{
-				foreach (var b in freeParamInterval.Split(Constants.ApproximationStepSize))
-				foreach (var k in freeParamInterval.Split(Constants.ApproximationStepSize))
-				{
-					var newParts = functionParts.ToArray();
-					var (left, _, _) = piecewiseFunction.Parts[0].Interval;
-					newParts[0] = piecewiseFunction.Parts[0] with
-					{
-						Function = _expressionParser.Parse($"(x - {left}) * {k} + {b}")
-					};
+			var (min, max) = piecewiseFunction.GetMinMaxValues();
+			return new Interval<decimal>((decimal) min, (decimal) max, IntervalInclusion.Closed);
+		}
 
-					if (_variationCalculator.GetVariation(new PiecewiseFunction(newParts[..1])) <= newVariation)
-						foreach (var function in Build(newParts, n + 1))
-							yield return function;
-				}
-			}
+		IEnumerable<PiecewiseFunction> Build(FunctionPart[] functionParts, double currentVariation, int index)
+		{
+			if (index == piecewiseFunction.Parts.Length)
+				yield return new PiecewiseFunction(functionParts);
+			else if (index == 0)
+				foreach (var b in GetFreeParameterRange().Split(Constants.ApproximationStepSize))
+				foreach (var function in GetVariants(functionParts, currentVariation, index,
+					         (double) piecewiseFunction.Parts[0].Interval.LeftValue, (double) b))
+					yield return function;
 			else
 			{
-				foreach (var k in freeParamInterval.Split(Constants.ApproximationStepSize))
-				{
-					var newParts = functionParts.ToArray();
-					var ((_, right, _), (func, _)) = newParts[n - 1];
-					newParts[n] = piecewiseFunction.Parts[n] with
-					{
-						Function = _expressionParser.Parse($"(x - {right}) * {k} + {func((double) right)}")
-					};
+				var (interval, (func, _)) = functionParts[index - 1];
+				var intervalStart = (double) interval.RightValue;
 
-					if (_variationCalculator.GetVariation(new PiecewiseFunction(newParts[..(n + 1)])) <= newVariation)
-						foreach (var function in Build(newParts, n + 1))
-							yield return function;
-				}
+				foreach (var function in
+				         GetVariants(functionParts, currentVariation, index, intervalStart, func(intervalStart)))
+					yield return function;
+			}
+		}
+
+		IEnumerable<PiecewiseFunction> GetVariants(IReadOnlyList<FunctionPart> functionParts, double currentVariation,
+			int index, double startX, double startY)
+		{
+			var paramInterval = piecewiseFunction.Parts[index].Interval.GetDiff<decimal, decimal>() > 0 &&
+			                    (decimal) (newVariation - currentVariation) is var remainingVariation
+				? new Interval<decimal>(-remainingVariation, remainingVariation, IntervalInclusion.Closed)
+				: new Interval<decimal>(0);
+
+			foreach (var k in paramInterval.Split(Constants.ApproximationStepSize))
+			{
+				var newParts = functionParts.ToArray();
+				newParts[index] = piecewiseFunction.Parts[index] with
+				{
+					Function = new RepresentableFunction(x => (x - startX) * (double) k + startY,
+						$"(x - {startX}) * {k} + {startY}")
+				};
+
+				foreach (var function in Build(newParts, currentVariation + (double) Math.Abs(k), index + 1))
+					yield return function;
 			}
 		}
 	}
---
+++ Domain/PiecewiseFunctionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Domain;
+
+public static class PiecewiseFunctionExtensions
+{
+	public static (double Min, double Max) GetMinMaxValues(this PiecewiseFunction piecewiseFunction) =>
+		piecewiseFunction.Range
+			.Split(0.1m)
+			.Select(x => piecewiseFunction.Evaluate((double) x))
+			.Aggregate((Min: double.MaxValue, Max: double.MinValue), (t, y) => (Math.Min(t.Min, y), Math.Max(t.Max, y)));
+}
